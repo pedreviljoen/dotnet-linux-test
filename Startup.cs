e2980789e41dec217976afe0eb74c4e515f43850
pedreviljoen <pedre87@gmail.com>
2020-07-31 11:18:22 +0200

    Change to test

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,7 @@ namespace aspnetcoreapp
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello from ASP.NET Core!");
+                return context.Response.WriteAsync("Hello this is Prod");
             });
         }
     }
